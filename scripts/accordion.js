6c34cdf0e8aeca2ff0b6676123bb1d836ef016d2
Shubham Mehta <shubhammehta.vc@flipkart.com>
2024-09-13 12:36:06 +0530

    [FIX] : Improved accordion component

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,6 +1,15 @@
-const ACCORDION_CONTENT = document.querySelectorAll(
+let ACCORDION_CONTENT = document.querySelectorAll(
   ".accordions .accordion-content-wrapper"
 );
+
+for (let wrapper of ACCORDION_CONTENT) {
+  const { clientHeight } = wrapper.querySelector(".accordion-content");
+  const duration = (clientHeight * 0.2) / 50;
+  wrapper.style.setProperty("--transition-duration", `${duration}s`);
+  wrapper.style.setProperty("--max-height", `${clientHeight}px`);
+}
+
+ACCORDION_CONTENT = document.querySelectorAll(".accordion-tray");
 
 for (let wrapper of ACCORDION_CONTENT) {
   const { clientHeight } = wrapper.querySelector(".accordion-content");
